36f4b7bc7e081df75ce36008d018c0f19f5b5377
rock000539 <rock000539@gmail.com>
2016-06-29 15:39:44 +0800

    test3

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -20,6 +20,7 @@ public class Servlet1 extends HttpServlet {
 		System.out.println("Servlet1  Servlet2");
 		System.out.println("Servlet1  Servlet2");
 		System.out.println("Servlet1  Servlet2");
+		System.out.println("Servlet1  Servlet5");
 		
 		HttpSession session = request.getSession();
 		session.setAttribute("time", ""+sDateFormat.format(new Date()));
