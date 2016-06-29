518bf067e072e11949e2f426bb63580b951792d4
ChiHao0913 <purplemaple0913@gmail.com>
2016-06-29 15:20:38 +0800

    modify Servlet1

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -15,14 +15,8 @@ public class Servlet1 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		
 		System.out.println(" Hello Servlet1");
-		
-
-
-		
 		System.out.println("Servlet1");
-
 
 		HttpSession session = request.getSession();
 		session.setAttribute("time", ""+sDateFormat.format(new Date()));
